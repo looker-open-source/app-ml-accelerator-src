b6c8a306660bdbd3a20914c4b5c468ed64c779b1
dan terwilliger <dtwigs@gmail.com>
2022-01-27 12:10:55 -0800

    Moves step complete button into a fixed footer bar, fixes some typing issues

--- src/components/StepComplete/StepComplete.tsx
+++ src/components/StepComplete/StepComplete.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { TrendingFlat } from "@styled-icons/material"
 import { WIZARD_STEPS } from "../../constants"
 import { useHistory} from 'react-router-dom'
+import { Button } from '@looker/components'
 
 type StepCompleteParams = {
   isStepComplete?: boolean
-  stepNumber: number
+  stepNumber: number,
+  stepText?: string
 }
 
-export const StepComplete: React.FC<StepCompleteParams> = ({ isStepComplete, stepNumber }) => {
+export const StepComplete: React.FC<StepCompleteParams> = ({ isStepComplete, stepNumber, stepText }) => {
   const history = useHistory()
 
   const handleClick = () => {
@@ -19,12 +20,14 @@ export const StepComplete: React.FC<StepCompleteParams> = ({ isStepComplete, ste
   const btnClass = isStepComplete ? 'complete' : ''
 
   return (
-    <div
-      className={`wizard-next-step-btn ${btnClass}`}
-      onClick={handleClick}
-    >
-      Continue
-      <TrendingFlat fontSize="small"/>
-    </div>
+    <>
+      <p>{stepText}</p>
+      <Button
+        className={`wizard-next-step-btn ${btnClass}`}
+        onClick={handleClick}
+      >
+        Continue
+      </Button>
+    </>
   )
 }
