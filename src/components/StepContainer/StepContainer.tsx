7507b513ea4e4800bbec286023111a1c545cb0ef
dan terwilliger <dtwigs@gmail.com>
2022-03-29 12:38:21 -0700

    Move advanced setting icon into bottom bar

--- src/components/StepContainer/StepContainer.tsx
+++ src/components/StepContainer/StepContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import LoadingOverlay from '../LoadingOverlay'
-import Spinner from '../Spinner'
 import StepComplete from '../StepComplete'
 
 type StepContainerParams = {
@@ -10,7 +9,7 @@ type StepContainerParams = {
   customClass?: string
   stepNumber: number
   children: any,
-  stepText?: string,
+  stepInfo?: any,
   buttonText?: string,
   handleCompleteClick?: () => Promise<boolean>
 }
@@ -22,7 +21,7 @@ export const StepContainer: React.FC<StepContainerParams> = ({
   customClass,
   stepNumber,
   children,
-  stepText,
+  stepInfo,
   buttonText,
   handleCompleteClick
 }) => {
@@ -34,8 +33,8 @@ export const StepContainer: React.FC<StepContainerParams> = ({
       { children }
       { !lastStep &&
         <div className="wizard-footer-bar">
+          {stepInfo}
           <StepComplete
-            stepText={stepText}
             isStepComplete={stepComplete}
             isLoading={isLoading}
             stepNumber={stepNumber}
