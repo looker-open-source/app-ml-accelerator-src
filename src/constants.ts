d9cd1ca5102d869cbf51f5be48f774f5045a6825
Liz Beacham <liz@4mile.io>
2022-07-02 16:30:28 -0700

    Correct constant value

--- src/constants.ts
+++ src/constants.ts
@@ -60,7 +60,7 @@ export const DESC_STRING = "desc"
 export const GOOGLE_SCOPES = 'https://www.googleapis.com/auth/bigquery'
 
 // Model and explore where the summary data lives
-export const BQML_LOOKER_MODEL = "bqml_extension"
+export const BQML_LOOKER_MODEL = "bqml_accelerator"
 export const SUMMARY_EXPLORE = "selection_summary"
 
 // user attribute keys from marketplace.json
